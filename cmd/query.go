39dff670e5ba758b54bbc343ab0b6a3e937d5e53
kai <kai@turbot.com>
2021-10-11 17:08:07 +0100

    working

--- cmd/query.go
+++ cmd/query.go
@@ -90,8 +90,6 @@ func runQueryCmd(cmd *cobra.Command, args []string) {
 		}
 	}()
 
-	db_local.StartPluginManager()
-
 	if stdinData := getPipedStdinData(); len(stdinData) > 0 {
 		args = append(args, stdinData)
 	}
